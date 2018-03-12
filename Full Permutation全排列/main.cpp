373c18d753d8a8a210922b5bd390d06bae1469ac
Uki Maxwell <yu_qiwei@zju.edu.cn>
2018-03-12 10:30:25 +0800

    改正了Generate1中的错误，增加了Generate2。 Generate1相比于Generate1提高了效率，Generate2进一步提高了效率，降低了代码复杂度。

--- Full Permutation全排列/main.cpp
+++ Full Permutation全排列/main.cpp
@@ -3,10 +3,10 @@
 #include <cstring>
 #include <cmath>
 const int maxn = 11;
-int n, P[maxn], hashTable[maxn] = { false };
+int n, P[maxn];
+bool hashTable[maxn] = { false };
 int count = 0;
 void Generate(int index){
-
 	if (index == n + 1){
 		for (int i = 1; i <= n; i++){
 			for (int j = i + 1; j <= n; j++){
@@ -33,7 +33,11 @@ void Generate(int index){
 	}
 };
 void Generate1(int index){
-	
+	for (int i = 1; i <= index - 2; i++){
+		if (abs(P[index - 1] - P[i]) == index - 1 - i){
+			return;
+		}
+	}
 	if (index == n + 1){
 		for (int i = 1; i <= n; i++){
 			printf("%d", P[i]);
@@ -42,23 +46,43 @@ void Generate1(int index){
 		count++;
 		return;
 	}
-	for (int i = 1; i <= index - 1; i++){
-		if (abs(P[index] - P[i]) == index - i){
-			return;
-		}
-	}
+	
 	for (int j = 1; j <= n; j++){
 		if (hashTable[j] == false){
 			hashTable[j] = true;
 			P[index] = j;
-			Generate(index + 1);
+			Generate1(index + 1);
+			hashTable[j] = false;
+		}
+	}
+};
+void Generate2(int index){
+	if (index == n + 1){
+		for (int i = 1; i <= n; i++){
+			printf("%d", P[i]);
+		}
+		printf("\n");
+		count++;
+		return;
+	}
+
+	for (int j = 1; j <= n; j++){
+		if (hashTable[j] == false){
+			hashTable[j] = true;
+			P[index] = j;
+			for (int k = 1; k < j; k++){
+				if (abs(P[j] - P[k]) == j - k){
+					break;
+				}
+			}
+			Generate1(index + 1);
 			hashTable[j] = false;
 		}
 	}
 };
 int main(){
-	n = 9;
-	Generate(1);
+	n = 8;
+	Generate2(1);
 	printf("%d\n", count);
 	system("pause");
 	return 0;
